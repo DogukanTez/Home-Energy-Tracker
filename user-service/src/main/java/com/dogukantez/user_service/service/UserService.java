ce14465b34c80740640fa6927ef3eb85d359ebbb
Doğukan Tez <dogukantez7@gmail.com>
2026-03-13 14:16:37 +0300

    feat: user create

--- user-service/src/main/java/com/dogukantez/user_service/service/UserService.java
+++ user-service/src/main/java/com/dogukantez/user_service/service/UserService.java
@@ -1,6 +1,8 @@
 package com.dogukantez.user_service.service;
 
 import com.dogukantez.user_service.dto.UserDto;
+import com.dogukantez.user_service.entity.User;
+import com.dogukantez.user_service.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +10,37 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
 
-    public UserDto createUser(UserDto userDto){
-        log.info("Creating user {}",userDto);
-        return userDto;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDto createUser(UserDto input){
+        log.info("Creating user {}",input);
+        final User createdUser = User.builder()
+                .name(input.getName())
+                .surname(input.getSurname())
+                .email(input.getEmail())
+                .address(input.getAddress())
+                .alerting(input.isAlerting())
+                .energyAlertingThreshold(input.getEnergyAlertingThreshold())
+                .build();
+
+        final User saved = userRepository.save(createdUser);
+        return toDto(saved);
+    }
+
+    private UserDto toDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .surname(user.getSurname())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .alerting(user.isAlerting())
+                .energyAlertingThreshold(user.getEnergyAlertingThreshold())
+                .build();
     }
 
 }
