e693abf735d0cdbb0991d451d4d925bfa2aa7f0b
Doğukan Tez <dogukantez7@gmail.com>
2026-03-13 13:20:17 +0300

    feat: base packages

---
+++ user-service/src/main/java/com/dogukantez/user_service/service/UserService.java
@@ -0,0 +1,16 @@
+package com.dogukantez.user_service.service;
+
+import com.dogukantez.user_service.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    public UserDto createUser(UserDto userDto){
+        log.info("Creating user {}",userDto);
+        return userDto;
+    }
+
+}
