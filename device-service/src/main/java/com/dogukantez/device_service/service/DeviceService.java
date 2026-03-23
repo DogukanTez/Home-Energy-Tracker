63c0bbb918999c993638bd0ad51e78ca6c5c7b5a
Doğukan Tez <dogukantez7@gmail.com>
2026-03-23 15:27:18 +0300

    feat: exceptions for device

--- device-service/src/main/java/com/dogukantez/device_service/service/DeviceService.java
+++ device-service/src/main/java/com/dogukantez/device_service/service/DeviceService.java
@@ -2,6 +2,7 @@ package com.dogukantez.device_service.service;
 
 import com.dogukantez.device_service.dto.DeviceDto;
 import com.dogukantez.device_service.entity.Device;
+import com.dogukantez.device_service.exception.DeviceNotFoundException;
 import com.dogukantez.device_service.repository.DeviceRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,7 @@ public class DeviceService {
     public DeviceDto getDeviceById(Long id) {
         Device device = deviceRepository.findById(id)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("Device not found with id " + id));
+                        new DeviceNotFoundException("Device not found with id " + id));
         return mapToDto(device);
     }
 
@@ -34,7 +35,7 @@ public class DeviceService {
     public DeviceDto updateDevice(Long id, DeviceDto input) {
         Device existing = deviceRepository.findById(id)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("Device not found with id " + id));
+                        new DeviceNotFoundException("Device not found with id " + id));
 
         existing.setName(input.getName());
         existing.setType(input.getType());
@@ -47,7 +48,7 @@ public class DeviceService {
 
     public void deleteDevice(Long id) {
         if (!deviceRepository.existsById(id)) {
-            throw new IllegalArgumentException("Device not found with id " + id);
+            throw new DeviceNotFoundException("Device not found with id " + id);
         }
         deviceRepository.deleteById(id);
     }
